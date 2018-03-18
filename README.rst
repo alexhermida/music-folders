95f83d7011e71b18df3de591f8bbe07e18e1aae5
Álex Hermida <alexhermida@gmail.com>
2018-03-18 22:59:07 +0100

    Update README & typo

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+.. image:: https://travis-ci.org/alexhermida/music-folders.svg?branch=master
+    :target: https://travis-ci.org/alexhermida/music-folders
+
 POC tool to set album names to music files
 ==========================================
 
--- tests.py
+++ tests.py
@@ -7,12 +7,12 @@ class Mp3TestCase(unittest.TestCase):
     """Tests for `mp3.py`."""
 
     def test_parser(self):
-        """Is generated paser correctly"""
+        """Is generated parser correctly"""
         parser = create_parser(['/localsytem/'])
         self.assertFalse(parser.overwrite_title)
 
     def test_parser_overwrite(self):
-        """Is generated paser correctly"""
+        """Is generated pasrer correctly"""
         parser = create_parser(['/localsytem/', '--overwrite_title'])
         self.assertTrue(parser.overwrite_title)
 
